eef175abfd1990b61f9bd5939eeb84b7a89f5410
Bamorph <142395807+Bamorph@users.noreply.github.com>
2023-11-14 12:08:58 +1100

    Create main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := flag.String("p", "9000", "Port to listen on")
+	directory := flag.String("d", ".", "Directory to serve files from")
+	flag.Parse()
+
+	server := &http.Server{
+		Addr:    ":" + *port,
+		Handler: http.FileServer(http.Dir(*directory)),
+	}
+
+	log.Printf("Serving files from directory %s on port %s\n", *directory, *port)
+	log.Fatal(server.ListenAndServe())
+}
